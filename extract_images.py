34d063b2009b40f7263a720dbc09658f3e7927cb
Peter Kok <kretep@users.noreply.github.com>
2023-05-30 16:16:44 +0200

    Crop image to region with image data

--- extract_images.py
+++ extract_images.py
@@ -13,8 +13,8 @@ def convert_image(file_in, file_out):
     ds = pydicom.read_file(file_in)
     
     # print tags
-    for element in ds:
-        print(element)
+#    for element in ds:
+#        print(element)
         
     # get image data
     img = ds.pixel_array
@@ -22,5 +22,23 @@ def convert_image(file_in, file_out):
     # convert to RGB
     img_rgb = convert_color_space(img, 'YBR_FULL_422', 'RGB')
     
+    # retrieve region with actual image data
+    region_data = ds[(0x0018, 0x6011)] # Sequence of Ultrasound Regions
+    for item in region_data:
+        x0 = get_value_from_sequence(item, (0x0018, 0x6018)) # Region Location Min X0
+        y0 = get_value_from_sequence(item, (0x0018, 0x601a)) # Region Location Min Y0
+        x1 = get_value_from_sequence(item, (0x0018, 0x601c)) # Region Location Min X1
+        y1 = get_value_from_sequence(item, (0x0018, 0x601e)) # Region Location Min Y1 
+    # print(x0, y0, x1, y1)
+   
+    # crop image to region
+    img_cropped = img_rgb[y0:y1, x0:x1]
+    
     # write image
-    cv2.imwrite(file_out, img_rgb)
+    cv2.imwrite(file_out, img_cropped)
+
+def get_value_from_sequence(item, tag):
+    data = item.get(tag)
+    if data is not None:
+        return data.value
+    
